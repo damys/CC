2c7490ca48857e04abe258daca784492507c909c
damys <damys@foxmail.com>
2016-09-29 09:31:21 +0800

    [add] framework/base/conf.php

---
+++ framework/base/Conf.php
@@ -0,0 +1,47 @@
+<?php
+
+class Conf
+{
+    static public $conf = array();
+
+    /**
+     * 加载系统配置,如果之前已经加载过,那么就直接返回
+     * @param $name 配置名
+     * @param string $file 文件名
+     * @return bool
+     */
+    static public function get($file='conf', $name){
+        if(isset(self::$conf[$file][$name])) {
+            return self::$conf[$file][$name];
+        }
+
+        $conf = ROOT . 'config' . DS . $file . '.php';
+
+        if(is_file($conf)) {
+            self::$conf[$file] = include $conf;
+            return isset(self::$conf[$file][$name]) ? self::$conf[$file][$name] : false;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * 加载系统配置文件(直接加载整个配置文件),如果之前已经加载过,那么就直接返回
+     * @param $file 文件名
+     * @return mixed
+     */
+    static public function getAll($file){
+        if(isset(self::$conf[$file])) {
+            return self::$conf[$file];
+        }
+
+        $conf = ROOT . 'config/' . $file . '.php';
+        if(is_file($conf)) {
+            self::$conf[$file] = include $conf;
+            return self::$conf[$file];
+        } else {
+            return false;
+        }
+    }
+}
