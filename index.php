327d227feffb799d286564eeb3550aaa5cd43158
damys <damys@foxmail.com>
2016-10-09 14:20:49 +0800

    [update] index.php

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ function microtime_float(){
 $d1 = microtime_float();
 
 
-
-
 define('DS', DIRECTORY_SEPARATOR);                //目录分隔符,自动匹配。win:\, unix:/
 define('ROOT', __DIR__ . DS);                     //根目录
 define('FRAMEWORK', ROOT . 'framework' . DS);     //框架目录
@@ -17,14 +15,14 @@ define('FRAMEWORK', ROOT . 'framework' . DS);     //框架目录
 /* ========================================================================
  *  URL 静态资源js, css, img 路径
  * ======================================================================== */
-define('SITE_URL', 'http://www.framework.com/');                  //网址域名,注意修改
-define('JS_URL', SITE_URL . 'assets/home/js/');            //前台静态资源路径,默认不带平台名
-define('CSS_URL', SITE_URL. 'assets/home/css/');
-define('IMG_URL', SITE_URL. 'assets/home/img/');
+define('SITE_URL', 'http://www.framework.com/');           //网址域名,注意修改
+define('JS_URL', SITE_URL . 'assets/js/');                 //前台静态资源路径,默认不带平台名
+define('CSS_URL', SITE_URL. 'assets/css/');
+define('IMG_URL', SITE_URL. 'assets/img/');
 define('ADMIN_JS_URL', SITE_URL . 'assets/admin/js/');     //后台静态资源路径
 define('ADMIN_CSS_URL', SITE_URL. 'assets/admin/css/');
 define('ADMIN_IMG_URL', SITE_URL. 'assets/admin/img/');
-define('IMG_UPLOAD', SITE_URL . 'upload/');                 //文件上传路径
+define('IMG_UPLOAD', SITE_URL . 'upload/');                //文件上传路径
 
 
 
