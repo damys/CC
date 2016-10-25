825112eac97890efca1cdf51ad3a96f1e863d278
damys <damys@foxmail.com>
2016-10-25 22:06:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-///////////////测试用////////////////////
+＜？php
 function microtime_float(){
     list($usec, $sec) = explode(' ', microtime());   //microtime() 返回：毫秒 时间戳 0.63559400 1469065900
     return ((float)$usec + (float)$sec);
@@ -15,7 +14,7 @@ define('FRAMEWORK', ROOT . 'framework' . DS);     //框架目录
 /* ========================================================================
  *  URL 静态资源js, css, img 路径
  * ======================================================================== */
-define('SITE_URL', 'http://www.framework.com/');           //网址域名,注意修改
+define('SITE_URL', 'http://www.abc.com/');           //网址域名,注意修改
 define('JS_URL', SITE_URL . 'assets/js/');                 //前台静态资源路径,默认不带平台名
 define('CSS_URL', SITE_URL. 'assets/css/');
 define('IMG_URL', SITE_URL. 'assets/img/');
@@ -28,7 +27,7 @@ define('IMG_UPLOAD', SITE_URL . 'upload/');                //文件上传路径
 
 include FRAMEWORK . 'init.php';
 
-///////////////测试用////////////////////
+
 $d2 = microtime_float();
 msg("动行消耗时间：" . ($d2-$d1));
 
