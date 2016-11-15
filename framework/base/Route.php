34d327b3600367a4a6e026aa8afec7bc0e5a6be9
damys <damys@foxmail.com>
2016-11-15 09:10:59 +0800

    [update route]

--- framework/base/Route.php
+++ framework/base/Route.php
@@ -13,35 +13,33 @@ class Route
 
         $uri = $_SERVER['REQUEST_URI'];
 
-        msg($uri);
-
         if (!empty($uri) && $uri != '/') {
             $path = explode('?',$uri);    // 丢掉?以及后面的参数
 
             // 去掉多余的分割符：/index/index => index, index
             $path = explode('/', trim($path[0],'/'));
 
-            //处理参数
+            //处理参数:对post, get 提交url 解析：/id/1/name/tom
             if (isset($path[0]) && $path[0]){
                 $this->controller = $path[0];
-                //unset($path[0]);                //卸掉控制器，方法名
+                unset($path[0]);                //卸掉控制器，方法名
             }
 
             if (isset($path[1]) && $path[1]) {
                 $this->action = $path[1];
-                //unset($path[1]);
+                unset($path[1]);
             } else {
                 $this->action = 'Index';
             }
 
-             //计算大于2个参数时，开始获取参数值
-//            $count = count($path) + 2;
-//            $i = 2;
-//            while ($i < $count){
-//                $_GET[$path[$i]] = $path[$i+1];
-//                $i = $i + 2;
-//            }
-//            msg($path);
+            //计算大于2个参数时，开始获取参数值
+            $count = count($path) + 2;
+            $i = 2;
+            while ($i < $count){
+                $_GET[$path[$i]] = $path[$i+1];
+                $i = $i + 2;
+            }
+            //msg($path);
 
 
         } else {
