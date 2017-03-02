2613c8122f68ff88a03572a9374c83d3bb184892
damys <damys@foxmail.com>
2017-03-02 09:49:17 +0800

    [update] Response

--- framework/base/Response.class.php
+++ framework/base/Response.class.php
@@ -2,11 +2,16 @@
 
 /* ========================================================================
  * 通信数据类
- * 主要功能, 按综合方式输出通信数据
+ * 主要功能：按综合方式输出通信数据
  * ======================================================================== */
 
-class Response {
+/**
+ * Class Response
+ */
+class Response
+{
     const JSON = "json";
+
     /**
      * 按综合方式输出通信数据
      * @param integer $code 状态码
@@ -15,17 +20,16 @@ class Response {
      * @param string $type 数据类型
      * return string
      */
-    public static function show($code, $message = '', $data = array(), $type = self::JSON) {
-        if(!is_numeric($code)) {
-            return '';
-        }
+    public static function show($code, $message = '', $data = array(), $type = self::JSON)
+    {
+        if(!is_numeric($code)) return '';
 
         $type = isset($_GET['format']) ? $_GET['format'] : self::JSON;
 
         $result = array(
-            'code' => $code,
+            'code'    => $code,
             'message' => $message,
-            'data' => $data,
+            'data'    => $data
         );
 
         if($type == 'json') {
@@ -37,9 +41,11 @@ class Response {
             self::xmlEncode($code, $message, $data);
             exit;
         } else {
-            // TODO
+            // TODO...
         }
     }
+
+
     /**
      * 按json方式输出通信数据
      * @param integer $code 状态码
@@ -47,21 +53,20 @@ class Response {
      * @param array $data 数据
      * return string
      */
-    public static function json($code, $message = '', $data = array()) {
-
-        if(!is_numeric($code)) {
-            return '';
-        }
+    public static function json($code, $message = '', $data = array())
+    {
+        if(!is_numeric($code)) return '';
 
         $result = array(
-            'code' => $code,
+            'code'    => $code,
             'message' => $message,
-            'data' => $data
+            'data'    => $data
         );
 
         echo json_encode($result);
         exit;
     }
+
 
     /**
      * 按xml方式输出通信数据
@@ -70,10 +75,9 @@ class Response {
      * @param array $data 数据
      * return string
      */
-    public static function xmlEncode($code, $message, $data = array()) {
-        if(!is_numeric($code)) {
-            return '';
-        }
+    public static function xmlEncode($code, $message, $data = array())
+    {
+        if(!is_numeric($code)) return '';
 
         $result = array(
             'code' => $code,
@@ -91,8 +95,14 @@ class Response {
         echo $xml;
     }
 
-    public static function xmlToEncode($data) {
 
+    /**
+     * 按xml 格式转换
+     * @param $data
+     * @return string
+     */
+    public static function xmlToEncode($data)
+    {
         $xml = $attr = "";
         foreach($data as $key => $value) {
             if(is_numeric($key)) {
@@ -107,29 +117,3 @@ class Response {
     }
 
 }
-
-
-/*
-test---
-public function check() {
-		$this->params['app_id'] = $appId = isset($_POST['app_id']) ? $_POST['app_id'] : '';
-		$this->params['version_id'] = $versionId = isset($_POST['version_id']) ? $_POST['version_id'] : '';
-		$this->params['version_mini'] = $versionMini = isset($_POST['version_mini']) ? $_POST['version_mini'] : '';
-		$this->params['did'] = $did = isset($_POST['did']) ? $_POST['did'] : '';
-		$this->params['encrypt_did'] = $encryptDid = isset($_POST['encrypt_did']) ? $_POST['encrypt_did'] : '';
-
-		if(!is_numeric($appId) || !is_numeric($versionId)) {
-			return Response::show(401, '参数不合法');
-		}
-		// 判断APP是否需要加密
-		$this->app = $this->getApp($appId);
-		if(!$this->app) {
-			return Response::show(402, 'app_id不存在');
-		}
-		if($this->app['is_encryption'] && $encryptDid != md5($did . $this->app['key'])) {
-			return Response::show(403, '没有该权限');
-		}
-	}
-
-
-*/
