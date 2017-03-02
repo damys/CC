9b110c7345751561979bc8a449df001321788856
damys <damys@foxmail.com>
2017-03-02 10:35:47 +0800

    [update] BaseController

--- app/admin/controller/BaseController.class.php
+++ app/admin/controller/BaseController.class.php
@@ -1,7 +1,17 @@
 <?php
 
+/**
+ * 基类
+ * 功能1：统一测试登录，权限过滤，强制退出
+ * 功能2：分配值，加载并显示页面，跳转， 防盗链，空处理
+ * Class BaseController
+ */
 class BaseController
 {
+
+    /* ========================================================================
+     * 功能2：分配值，加载并显示页面，跳转， 防盗链，空处理
+     * ======================================================================== */
 
     /**
      * 分配值
@@ -9,7 +19,9 @@ class BaseController
      * @param $value
      */
     public $assign = array();
-    function assign($name, $value){
+
+    function assign($name, $value)
+    {
         $this->assign[$name] = $value;
     }
 
@@ -18,7 +30,8 @@ class BaseController
      * 加载并显示页面
      * @param $file
      */
-    function display($file){
+    function display($file)
+    {
         $file = VIEW_PATH . $file;
 
         if(is_file($file)){
@@ -27,20 +40,23 @@ class BaseController
         }
     }
 
+
     /**
      * 跳转
      * @param $str
      */
-    function redirect($url){
+    function redirect($url)
+    {
         header('Location:' . $url);
     }
+
 
     /**
      * 防盗链
      * @param string $error_url 可以指定错误跳转url
      */
-    public function referer($url = ''){
-
+    public function referer($url = '')
+    {
         if(isset($_SERVER['HTTP_REFERER'])){
 
             // 拼装Http
@@ -61,22 +77,39 @@ class BaseController
     /**
      * 空方法操作的处理, 子类可以重写些方法
      */
-    public function _empty(){
-        $this->referer(SITE_URL);
+    public function _empty()
+    {
+        die("空方法操作");
     }
+
 
     /**
      * 魔术方法 在不存在的操作的时候执行, 并调用_empty 方法
-     * @access public
+     * @access assets
      * @param string $method 方法名
      * @param array $args 参数
      * @return mixed
      */
-    public function __call($method,$args) {
+    public function __call($method,$args)
+    {
         if(method_exists($this, '_empty')){
             $this->_empty($method, $args);
         }
 
         return;
+    }
+
+
+    /**
+     * @param $msg 显示的提示信息
+     * @param $url 跳转的url 地址
+     * @param int $time 页面的跳转时间，默认为3秒
+     */
+    function gotoUrl($msg, $url, $time=3)
+    {
+        echo $msg;
+        echo "<a href='{$url}'>返回</a>";
+        echo "<br>页面将在{$time}秒之后自动跳转";
+        header("refresh: $time; url = $url");
     }
 }
