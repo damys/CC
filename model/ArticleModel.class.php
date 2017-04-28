dd444809e5ba12816770130585693ff968bc4f79
damys <damys@foxmail.com>
2017-04-28 11:26:58 +0800

    [update]

--- model/ArticleModel.class.php
+++ model/ArticleModel.class.php
@@ -5,7 +5,17 @@
  */
 class ArticleModel extends BaseModel
 {
-    private $_table = 'article';
+    // 数据表名
+    private $_table = null;
 
 
+    /**
+     * 继承父类的方法, 从配置文件获取数据表前缀名
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->_table = $this->_db_prefix . "article";
+    }
 }
