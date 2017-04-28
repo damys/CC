415adb7ecc173b8cc7db98c69b9d1b90c88067b2
damys <damys@foxmail.com>
2017-04-28 11:30:28 +0800

    [update function]

--- framework/base/function.php
+++ framework/base/function.php
@@ -15,7 +15,7 @@ function msg($var)
     } else if (is_null($var)) {
         var_dump(NULL);
     } else {
-        echo "<pre style='position:relative; _position:fixed; bottom:0; left: 0; ;z-index:1000;padding:10px;border-radius:4px;background:#F5F5F5;border:1px solid #ddd;font-size:14px;line-height:18px;'>" . print_r($var, true) . "</pre>";
+        echo "<pre style='position:relative; _position:fixed; bottom:0; left: 0; ;z-index:1000;padding:10px;border-radius:4px;background:#F5F5F5;border:1px solid #ddd;font-size:14px;line-height:18px;'>" .  print_r($var, true) . "</pre>";
     }
 }
 
@@ -76,4 +76,56 @@ function post($str=false, $filter = '', $default = false)
     } else {
         return $_POST;
     }
+}
+
+
+/**
+ * 截取字符串，后加点。 注：一个中文占3个字符
+ * @param $str
+ * @param $len
+ * @param string $ellipsis
+ * @return string
+ */
+function sub_str($str, $len, $ellipsis = '...')
+{
+    if( mb_strlen($str, 'utf-8') <= $len) return $str;
+    else return mb_substr($str, 0, $len, 'utf-8') . $ellipsis;
+}
+
+
+/**
+ * 获取和设置语言定义(不区分大小写)
+ * @param string|array $name 语言变量
+ * @param mixed $value 语言值或者变量
+ * @return mixed
+ */
+function L($name=null, $value=null)
+{
+    static $_lang = array();
+
+    // 空参数返回所有定义
+    if (empty($name))
+        return $_lang;
+    // 判断语言获取(或设置)
+    // 若不存在,直接返回全大写$name
+    if (is_string($name)) {
+        $name   =   strtoupper($name);
+        if (is_null($value)){
+            return isset($_lang[$name]) ? $_lang[$name] : $name;
+        }elseif(is_array($value)){
+            // 支持变量
+            $replace = array_keys($value);
+            foreach($replace as &$v){
+                $v = '{$'.$v.'}';
+            }
+            return str_replace($replace,$value,isset($_lang[$name]) ? $_lang[$name] : $name);
+        }
+        $_lang[$name] = $value; // 语言定义
+        return null;
+    }
+    // 批量定义, 合并数组，转为大写
+    if (is_array($name))
+        $_lang = array_merge($_lang, array_change_key_case($name, CASE_UPPER));
+
+    return null;
 }
