59cf279fa18482434857d6e9011449f3868e4c77
damys <damys@foxmail.com>
2017-08-12 12:53:47 +0800

    [update]

--- framework/base/Route.class.php
+++ framework/base/Route.class.php
@@ -16,10 +16,11 @@ class Route
     /**
      * 获取URL 参数部分
      * 返回对应控制器和方法
+     * 注： 支持linux（严格区分大小写）路由
      */
     public function __construct()
     {
-        $uri = strtolower($_SERVER['REQUEST_URI']);
+        $uri = $_SERVER['REQUEST_URI'];
 
         if (!empty($uri) && $uri != '/') {
             $path = explode('?',$uri);    // 丢掉?以及后面的参数
@@ -27,29 +28,24 @@ class Route
             /**
              * 一、controller， action 路由设置
              */
-            // 3-1. 去掉多余的分割符：/index/index => index, index
+            // 3-1. 去掉多余的分割符：/index/index => Index, Index
             $path = explode('/', trim($path[0],'/'));
 
             // 3-2.处理参数:controller
             if (isset($path[0]) && $path[0]){
-                // controller伪静态：可以加.html
-                if(strpos($path[0], '.')){
-                    $path[0] = substr($path[0],0, strpos($path[0], '.'));
-                }
-                $this->controller = $path[0];
+                $this->controller = ucfirst($path[0]);
             }
 
             // 3-3.处理参数:action 默认为：Index
             if (isset($path[1]) && $path[1]) {
-                // action伪静态：可以加.html
+                // 伪静态：可以加.html
                 if(strpos($path[1], '.')){
                     $path[1] = substr($path[1],0, strpos($path[1], '.'));
                 }
-                $this->action = $path[1];
+                $this->action = strtolower($path[1]);
             } else {
-                $this->action = $GLOBALS['CONF']['DEF_ACTION'];
+                $this->action = strtolower($GLOBALS['CONF']['DEF_ACTION']);
             }
-
 
             /**
              * 短域名设置，美化域名，通过获取配置文件设置
@@ -57,9 +53,10 @@ class Route
              * 注意：短名最好不要与控制器重名
              */
             // 短域名设置
-            if(isset($GLOBALS['CONF']['ROUTE'][$this->controller])){
-                $this->action = $GLOBALS['CONF']['ROUTE'][$this->controller][1];
-                $this->controller = $GLOBALS['CONF']['ROUTE'][$this->controller][0];
+            if(isset($GLOBALS['CONF']['Route.class'][$this->controller])){
+
+                $this->action = $GLOBALS['CONF']['Route.class'][$this->controller][1];
+                $this->controller = ucfirst($GLOBALS['CONF']['Route.class'][$this->controller][0]);
 
                 // 短域名设置的路由数组首位缺少一位，因为短域名缺少一位
                 array_unshift($path, 0);
@@ -91,12 +88,11 @@ class Route
                 $_GET[$path[$i]] = $path[$i+1];
                 $i = $i + 2;
             }
-
         } else {
             // 如首页缺控制器，默认调用
-            $this->controller = $GLOBALS['CONF']['DEF_CONTROLLER'];
-            $this->action = $GLOBALS['CONF']['DEF_ACTION'];
+            $this->controller = ucfirst($GLOBALS['CONF']['DEF_CONTROLLER']);
+            $this->action = strtolower($GLOBALS['CONF']['DEF_ACTION']);
         }
-        // msg("实际调用：controller:" . $this->controller . ', ' . 'action:' . $this->action);
+        //msg("实际调用：controller:" . $this->controller . ', ' . 'action:' . $this->action);
     }
 }
