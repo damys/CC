8b28fa6bda2a44ce27df1395ca67a78c2347aac1
damys <damys@foxmail.com>
2017-09-04 11:01:32 +0800

    [update function.php]

--- framework/base/function.php
+++ framework/base/function.php
@@ -129,35 +129,3 @@ function L($name=null, $value=null)
 
     return null;
 }
-
-
-/**
- * 按时间戳格式化自定义时间
- * @param $time
- * @return bool|string
- */
-function formatTime($time)
-{
-    $rtime = date("Y-m-d H:i",$time);
-    $time = time() - $time;
-
-    if ($time < 60){
-        $str = '刚刚';
-    }elseif($time < 60 * 60){
-        $min = floor($time/60);
-        $str = $min.'分钟前';
-    }elseif($time < 60 * 60 * 24){
-        $h = floor($time/(60*60));
-        $str = $h.'小时前 ';
-    }elseif($time < 60 * 60 * 24 * 3){
-        $d = floor($time/(60*60*24));
-        if($d==1){
-            $str = '昨天 '.$rtime;
-        }else{
-            $str = '前天 '.$rtime;
-        }
-    }else{
-        $str = $rtime;
-    }
-    return $str;
-}
