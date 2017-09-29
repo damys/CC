d6f1101d948416402e0fd01cfca02c0aa233c761
damys <damys@foxmail.com>
2017-09-29 10:36:58 +0800

    [add]formatTime

--- framework/library/function.php
+++ framework/library/function.php
@@ -103,7 +103,7 @@ function getdaysInmonth($month, $year)
  * @param  number $time 时间戳
  * @return string     返回多少以前
  */
- function formateTime($time) {
+ function formatTime($time) {
     $time = (int) substr($time, 0, 10);
     $int = time() - $time;
     $str = '';
@@ -122,3 +122,36 @@ function getdaysInmonth($month, $year)
     }
     return $str;
 }
+
+
+
+/**
+ * 按时间戳格式化自定义时间
+ * @param $time
+ * @return bool|string
+ */
+ function formatTime($time)
+ {
+     $rtime = date("Y-m-d H:i",$time);
+     $time = time() - $time;
+ 
+     if ($time < 60){
+         $str = '刚刚';
+     }elseif($time < 60 * 60){
+         $min = floor($time/60);
+         $str = $min.'分钟前';
+     }elseif($time < 60 * 60 * 24){
+         $h = floor($time/(60*60));
+         $str = $h.'小时前 ';
+     }elseif($time < 60 * 60 * 24 * 3){
+         $d = floor($time/(60*60*24));
+         if($d==1){
+             $str = '昨天 '.$rtime;
+         }else{
+             $str = '前天 '.$rtime;
+         }
+     }else{
+         $str = $rtime;
+     }
+     return $str;
+ }
