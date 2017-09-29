48524b0e500911ed0c8253486b10752de64ef3c1
damys <damys@foxmail.com>
2017-09-29 10:35:35 +0800

    [add] formateTime

--- framework/library/function.php
+++ framework/library/function.php
@@ -95,3 +95,30 @@ function getdaysInmonth($month, $year)
     }
     return ($days);
 }
+
+
+
+/**
+ * 传入时间戳,计算距离现在的时间
+ * @param  number $time 时间戳
+ * @return string     返回多少以前
+ */
+ function formateTime($time) {
+    $time = (int) substr($time, 0, 10);
+    $int = time() - $time;
+    $str = '';
+    if ($int <= 2){
+        $str = sprintf('刚刚', $int);
+    }elseif ($int < 60){
+        $str = sprintf('%d秒前', $int);
+    }elseif ($int < 3600){
+        $str = sprintf('%d分钟前', floor($int / 60));
+    }elseif ($int < 86400){
+        $str = sprintf('%d小时前', floor($int / 3600));
+    }elseif ($int < 1728000){
+        $str = sprintf('%d天前', floor($int / 86400));
+    }else{
+        $str = date('Y-m-d H:i:s', $time);
+    }
+    return $str;
+}
