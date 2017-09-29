1cf526e1920ca650378d0924fa86b9c031d75ce6
damys <damys@foxmail.com>
2017-09-29 10:35:02 +0800

    [add] getdaysInmonth

--- framework/library/function.php
+++ framework/library/function.php
@@ -41,7 +41,7 @@ function random($type='', $length = 32)
 * @return string
 * @return string
 */
-function make_rand($type='', $length=32)
+function random($type='', $length=32)
 {
    if($type === 'touble'){
        $chars=array(
@@ -70,4 +70,28 @@ function make_rand($type='', $length=32)
        $password.=$chars[$keys[$i]];
    }
    return $password;
+}
+
+
+/**
+ * 计算该月有几天
+ * @param $month 
+ * @param $year
+ * @return int
+*/
+function getdaysInmonth($month, $year) 
+{
+    $days = '';
+    if ($month == 1 || $month == 3 || $month == 5 || $month == 7 || $month == 8 || $month == 10 || $month == 12)
+        $days = 31;
+    else if ($month == 4 || $month == 6 || $month == 9 || $month == 11)
+        $days = 30;
+    else if ($month == 2) {
+        if (isLeapyear($year)) {
+            $days = 29;
+        } else {
+            $days = 28;
+        }
+    }
+    return ($days);
 }
