40cb98241c0ec5c08a28845d7d2ed3448b37b215
damys <damys@foxmail.com>
2018-03-01 10:07:01 +0800

    update

--- app/home/controller/IndexController.class.php
+++ app/home/controller/IndexController.class.php
@@ -4,12 +4,25 @@ class IndexController extends BaseController
 {
     public function IndexAction(){
 
-        $model = ModelFactory::M('IndexModel');
-        msg($model->getRow());
+//        $model = ModelFactory::M('IndexModel');
+//        msg($model->getRow());
+        $model = ModelFactory::M('ArticleModel');
+        msg($model->getAll());
 
-        $title = 'home title';
 
-        $this->assign('title', $title);
+
+//        msg(Route::getInstance());
+
+        $this->assign('title', "home title");
         $this->display('/index/index.html');
+    }
+	
+	public function testAction()
+    {
+        include FRAMEWORK . 'captcha'.DS.'Captcha.class.php';
+
+        $captcha = new Captcha();
+        $captcha->viewImg();
+        $_SESSION['captcha'] = $captcha->getCode();
     }
 }
